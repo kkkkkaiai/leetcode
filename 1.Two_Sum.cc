227d16f747246ec2a4b475def46701c1d9e43ccd
Warren <chenkai0130@outlook.com>
2019-01-28 03:54:05 -0800

    change

--- 1.Two_Sum.cc
+++ 1.Two_Sum.cc
@@ -7,23 +7,21 @@ using namespace std;
 
 
 class Solution {
-	public:
-		vector<int> twoSum(vector<int>& nums, int target) {
+public:
+	vector<int> twoSum(vector<int>& nums, int target) {
+		unordered_map<int, int> find;
+		vector<int> ans;
 
-			unordered_map<int, int> find;
-			vector<int> ans;
-
-			for(unsigned int i = 0; i < nums.size(); ++i){
-				if(find.count(target - nums[i])){
-					ans.push_back(find[target - nums[i]]);
-					ans.push_back(i);
-				}else{
-					find.insert({nums[i], i});
-				}
+		for(unsigned int i = 0; i < nums.size(); ++i){
+			if(find.count(target - nums[i])){
+				ans.push_back(find[target - nums[i]]);
+				ans.push_back(i);
+			}else{
+				find.insert({nums[i], i});
 			}
-
-
-			return ans;
 		}
+
+		return ans;
+	}
 };
 
