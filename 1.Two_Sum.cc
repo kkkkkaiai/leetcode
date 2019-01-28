5c589eb7fe057a5e38e40c227ed8b4f0380a137d
Warren <chenkai0130@outlook.com>
2019-01-28 03:51:28 -0800

    1 solution

---
+++ 1.Two_Sum.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <map>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+
+
+class Solution {
+	public:
+		vector<int> twoSum(vector<int>& nums, int target) {
+
+			unordered_map<int, int> find;
+			vector<int> ans;
+
+			for(unsigned int i = 0; i < nums.size(); ++i){
+				if(find.count(target - nums[i])){
+					ans.push_back(find[target - nums[i]]);
+					ans.push_back(i);
+				}else{
+					find.insert({nums[i], i});
+				}
+			}
+
+
+			return ans;
+		}
+};
+
